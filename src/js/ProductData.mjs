eac6eed29723d68cad1048a09ba3b6c5064800bd
Joaquin Solis <65245306+solisjoaquin@users.noreply.github.com>
2022-11-18 20:43:35 -0300

    Initialize vite-team5

---
+++ src/js/ProductData.mjs
@@ -0,0 +1,25 @@
+const baseURL = "http://server-nodejs.cit.byui.edu:3000/";
+function convertToJson(res) {
+  if (res.ok) {
+    return res.json();
+  } else {
+    throw new Error("Bad Response");
+  }
+}
+
+export default class ProductData {
+  constructor(category) {
+    // this.category = category;
+    // this.path = `../json/${this.category}.json`;
+  }
+  async getData(category) {
+    const response = await fetch(baseURL + `products/search/${category}`);
+    const data = await convertToJson(response);
+    return data.Result;
+  }
+  async findProductById(id) {
+    const response = await fetch(baseURL + `product/${id}`);
+    const data = await convertToJson(response);
+    return data.Result;
+  }
+}
